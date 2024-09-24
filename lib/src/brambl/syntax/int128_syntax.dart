173df14b7f16baedac1824ac3e2ded28372e9935
Selby Baidjnath <selby@serubii.com>
2024-09-24 07:33:59 -0300

    added various Int128 helper/extensions
    - added Modulo and Mult to Int128
    - added direct conversion to Int128 from Uint32Value

--- lib/src/brambl/syntax/int128_syntax.dart
+++ lib/src/brambl/syntax/int128_syntax.dart
@@ -1,8 +1,15 @@
+import 'package:topl_common/proto/google/protobuf/wrappers.pb.dart';
 import 'package:topl_common/proto/quivr/models/shared.pb.dart';
 
 import '../../../brambldart.dart';
 
 /// Int 128 syntax extensions
+
+extension Int128Syntax on Int128 {
+  Int128 get zero => Int128(value: [0, 0, 0, 0]); //// 4 int zeroes
+
+  int toInt() => value.toBigInt.toInt();
+}
 
 extension Int128AsBigInt on Int128 {
   BigInt toBigInt() => value.toBigInt;
@@ -43,17 +50,39 @@ extension Int128Operations on Int128 {
     return toBigInt() <= other.toBigInt();
   }
 
+  Int128 operator *(Int128 other) {
+    final result = toBigInt() * other.toBigInt();
+    return result.toInt128();
+  }
+
+  Int128 operator %(Int128 other) {
+    final thisBigInt = toBigInt();
+    final otherBigInt = other.toBigInt();
+    final resultBigInt = thisBigInt % otherBigInt;
+    return resultBigInt.toInt128();
+  }
+
   String get show => toBigInt().toString();
+
 }
 
 // dart shorthand instead of reduce
 extension IterableInt128SumExtension on Iterable<Int128> {
-
   /// Returns the sum of all elements in the iterable.
   Int128 sum() {
     if (isEmpty) {
       throw StateError('Cannot sum elements of an empty iterable');
     }
     return reduce((value, element) => value + element);
+  }
+}
+
+extension Uint32Extensions on UInt32Value {
+  Int128 toInt128() {
+    // Access the underlying integer value
+    final int intValue = value;
+
+    // Convert to Int128
+    return intValue.toInt128();
   }
 }
