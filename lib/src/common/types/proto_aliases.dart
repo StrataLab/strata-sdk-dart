62ab2112c9b6e3ed4c302dbbb3c7ebdbb5d6454e
Selby Baidjnath <selby@serubii.com>
2024-09-24 10:32:40 -0300

    added boxvalue alias

--- lib/src/common/types/proto_aliases.dart
+++ lib/src/common/types/proto_aliases.dart
@@ -1,12 +1,12 @@
 import 'package:topl_common/proto/brambl/models/box/value.pb.dart';
 
-
 /// simplify protobuf imports with these typedefs
 /// TODO: replace global occurences with new typedefs
 
+typedef BoxValue = Value;
+
 typedef LVL = Value_LVL;
 typedef Asset = Value_Asset;
-// typedef Value = Value_Value; // TODO: disabled because it overlaps with BoxValue
 typedef Group = Value_Group;
 typedef Series = Value_Series;
 typedef TOPL = Value_TOPL;
